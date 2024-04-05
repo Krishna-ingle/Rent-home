aa47a5c3c274d44b229b3d16a52f25c25790df84
krishna-ingle <inglekrishna05@gmail.com>
2024-04-05 19:12:06 +0530

    change_script_location

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 let btn1 = document.querySelector("#btn1");
 
 btn1.onclick = () => {
-	console.log("hiii");
+	let card =document.querySelector("#card");
+	card.style.visibility = "visible";
 }
 console.log(btn1);
+
+$('#service_2').addClass("active");
+$("#service_2").click(function() {
+	$('html, body') .animate({
+		scrollTop:$("#tool_service").offset().top-112
+	}, 5000);
+ return false;
+});
+
